c859958dcfdf89dfe9bb1a5aa122aedabf79f01e
htejo <hector.tejo@gmail.com>
2025-07-21 17:08:42 -0400

    componente Home

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main content of the home page.</p>
+        </div>
+    );
+}
